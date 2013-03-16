95125d6c5c70c33f76b931bbafa7d75996adb140
Squid <squid12345678@gmail.com>
2013-03-16 11:05:52 +0000

    added mame.cfg

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 cd /usr/local/bin/indiecity/InstalledApps/mame4all_pi/Full
-chmod 777 ./samples ./artwork ./cfg ./inp ./snap ./hi ./roms ./nvram ./skins ./memcard ./frontend
+chmod 777 ./mame.cfg ./samples ./artwork ./cfg ./inp ./snap ./hi ./roms ./nvram ./skins ./memcard ./frontend
