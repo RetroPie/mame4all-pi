c6dcd7d1aab234792bf7f470254f39b82fa3a0fd
J. Tang <tang@jtang.org>
2020-11-20 15:21:39 -0500

    cheat,m68k: Fix NULL references
    NULL is a pointer to 0, and is not directly assignable to a non-pointer
    variable. Replace "NULL" with 0 when storing in a non-pointer.

--- src/cheat.cpp
+++ src/cheat.cpp
@@ -2125,7 +2125,7 @@ INT32 ViewSearchResults (struct osd_bitmap *bitmap, INT32 selected)
 
 void RestoreSearch (void)
 {
-	int restoreString = NULL;	/* Steph */
+	int restoreString = 0;	/* Steph */
 
 	switch (restoreStatus)
 	{
